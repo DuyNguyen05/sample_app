bdc3cd5b9947c4ba40b254065d8c80d199a5658f
Duy Nguyen <nguyenngocduy.84@live.com>
2018-12-01 18:06:01 +0700

    Them method new , create trong UserController . Show,New file in View

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root "static_pages#home"
-get "/static_pages/help"
-get "/static_pages/about"
+  get "/static_pages/contact"
+  get "/static_pages/home"
+  get "/static_pages/help"
+  get "/static_pages/about"
 
-#get "/users/new" , to:"users#new"
-get "/users/:id" , to:"users#show"
-
-#post "/users" , to: "user#create"
+  get "/users/new", to:"users#new"
+  get "/users/:id", to:"users#show"
+  post "/users", to:"users#create"
 
 
 #resources :users
