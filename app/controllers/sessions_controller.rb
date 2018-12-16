53980446d060439074b1e770ea8c3c252726a3e7
Duy Nguyen <nguyenngocduy.84@live.com>
2018-12-16 23:38:50 +0700

    Log out and log in with cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      flash[:success] = "Welcome Back!"
+      log_in user
+      params[:session][:remember_me] == '1'? remember(user) : forget(user)
+      flash[:success] = "Xin Chao #{current_user.name} "
       redirect_to "/users/#{user.id}"
     else
       flash[:danger] = "Oops!! Please Try Again"
@@ -15,7 +17,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
+    log_out
+    flash[:info] = "See You!"
+    redirect_to root_path
   end
 
 
